dd24773503682ede44d15b5a26e9874158ac459e
marcusdickens <marcus.r.dickens@gmail.com>
2022-06-08 11:49:26 -0400

    styling to chat container video container and icons

--- src/components/MessagingContainer.js
+++ src/components/MessagingContainer.js
@@ -1,10 +1,12 @@
-import {React} from 'react'
+import {React,useState} from 'react'
 import {useCookies} from 'react-cookie'
 import { Window,ChannelHeader,MessageList,MessageInput,Thread} from 'stream-chat-react'
 import UserList from './UserList'
+import {FaUsers, FaArrowAltCircleLeft} from 'react-icons/fa'
 
  function MessagingContainer({users}) {
     const [cookies, setCookies, removeCookies] = useCookies(['user'])
+    const [userListVisible, setUserListVisible] = useState(false)
 
      const logout = () => {
          removeCookies('Name', cookies.Name)
@@ -16,13 +18,27 @@ import UserList from './UserList'
 
     return(
         <div className='messaging-container'>
-            <Window>
-                <ChannelHeader />
-                <MessageList />
-                <MessageInput />
-                <button className='standard-button'onClick={logout}>Log Out</button>
-                <UserList users={users} />
-            </Window>
+            {!userListVisible && (
+
+                <Window>
+                    <FaUsers className='icon' onClick={() => setUserListVisible(true)}/>
+                    <ChannelHeader />
+                    <MessageList />
+                    <MessageInput />
+                    <button className='standard-button'onClick={logout}>Log Out</button>
+                </Window>
+            )}
+
+            {userListVisible && (
+                <Window>
+                    <div className='chat-container'>
+                        <FaArrowAltCircleLeft className='icon' onClick={() => setUserListVisible(false)} />
+                        <ChannelHeader  title='users'/>
+                        <UserList users={users} />
+                    </div>
+                </Window>
+            )}
+            
             <Thread />
         </div>
     )
