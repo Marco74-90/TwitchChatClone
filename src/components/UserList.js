3a7b97e949ce16e73fac2ca96611390487313d2a
marcusdickens <marcus.r.dickens@gmail.com>
2022-06-08 11:22:00 -0400

    styling for user list

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {FaHorseHead, FaSmile} from 'react-icons/fa'
 
 export default function UserList ({users}){
 
     return(
 
-        <div>
+        <div className="userList-container">
+            {users?.map(user =>(
+                <li key={user.id}>
+                    {user.role == 'user' ? <FaHorseHead/> : <FaSmile/>}
+                    <p>{user.name}</p>
+                </li>
+            ))}
 
         </div>
 
